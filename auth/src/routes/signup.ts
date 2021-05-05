4fba36f9a8bd2836cffffc7054991bfd5a38f121
chanakaHetti <cdileep40@gmail.com>
2021-05-05 22:59:33 +0530

    Development

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-erros';
@@ -32,13 +33,26 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      console.log('Email in use');
-      return res.send({});
-      // throw new BadRequestError('Email in use!');
+      // console.log('Email in use');
+      // return res.send({});
+      throw new BadRequestError('Email in use!');
     }
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Genarate json web token
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    },
+      process.env.JWT_KEY!
+    );
+
+    // Store jwt on session object
+    req.session = {
+      jwt: userJwt
+    };
 
     res.status(201).send(user);
   }
